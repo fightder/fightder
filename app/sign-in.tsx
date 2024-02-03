d2811a73b5cf07bbdfd2746a28b5d35e0c6be942
Bunyasit Fang <wachaa1319@gmail.com>
2024-02-03 13:01:06 +0700

    pls build

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -8,12 +8,30 @@ import { Button } from "components/Button";
 import { SafeBottom, SafeTop } from "components/SafeTop";
 import { Image } from "react-native";
 import SignInForm from "components/SignInForm";
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+  statusCodes,
+} from "@react-native-google-signin/google-signin";
+
+import {
+  AppleAuthenticationButton,
+  AppleAuthenticationButtonStyle,
+  AppleAuthenticationButtonType,
+  AppleAuthenticationScope,
+  isAvailableAsync,
+  signInAsync,
+} from "expo-apple-authentication";
 
 export default function SignIn() {
   const { signIn, isLoading, session } = useSession();
   // const [signUp, setSignUp] = useState(false);
+  const available = isAvailableAsync().then((res) => {
+    console.log(res, "res");
+  });
   useEffect(() => {
     console.log(session, isLoading);
+    console.log(available, "av");
   }, [session, isLoading]);
 
   return (
@@ -32,8 +50,36 @@ export default function SignIn() {
         source={require("assets/icon.png")}
       />
       <SignInForm />
-
+      <AppleAuthenticationButton
+        buttonType={AppleAuthenticationButtonType.SIGN_IN}
+        buttonStyle={AppleAuthenticationButtonStyle.BLACK}
+        cornerRadius={5}
+        onPress={async () => {
+          try {
+            const credential = await signInAsync({
+              requestedScopes: [
+                AppleAuthenticationScope.FULL_NAME,
+                AppleAuthenticationScope.EMAIL,
+              ],
+            });
+            // signed in
+          } catch (e) {
+            if (e.code === "ERR_REQUEST_CANCELED") {
+              // handle that the user canceled the sign-in flow
+            } else {
+              // handle other errors
+            }
+          }
+        }}
+      />
       <View flex style={{ marginBottom: "auto" }} />
+      <GoogleSigninButton
+        size={GoogleSigninButton.Size.Wide}
+        color={GoogleSigninButton.Color.Dark}
+        onPress={this._signIn}
+        disabled={this.state.isSigninInProgress}
+      />
+      ;
       <Button
         onPress={async () => {
           await signIn();
